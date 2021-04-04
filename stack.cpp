594644a9cde994751afd0f9f2e733013bc8b3f24
nick <trojanhorse0077@gmail.com>
2021-04-04 10:51:28 +0800

    add stack calcPostfix

--- stack.cpp
+++ stack.cpp
@@ -37,17 +37,106 @@ int pop(slink stack) {
     return stack->data[stack->sp--];
 }
 
-int main() {
+
+char *expr= {"62/3-42*+"};
+
+typedef enum Precedence {
+    Lparen,
+    Rparen,
+    Plus,
+    Minus,
+    Times,
+    Divide,
+    Mod,
+    Eos,
+    Operand
+};
+
+Precedence getToken(char *symbol, int *n) {
+
+    *symbol = expr[(*n)++];
+
+    switch (*symbol) {
+        case '(':
+            return Lparen;
+        case ')':
+            return Rparen;
+        case '+':
+            return Plus;
+        case '-':
+            return Minus;
+        case '/':
+            return Divide;
+        case '*':
+            return Times;
+        case '%':
+            return Mod;
+        case '\0':
+            return Eos;
+        default:
+            return Operand;
+
+    }
+}
+
+
+/**
+ * 計算後置運算式 expr
+ * @return
+ */
+int calcPostfix() {
+    Precedence token;
+    char symbol;
+    int op1, op2;
+    int n = 0;
+    token = getToken(&symbol, &n);
     slink stack = createStack(5);
-    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int i = 0;
-    while (!full(stack))
-        push(stack, data[i++]);
-    while (!empty(stack))
-        printf("%d\n", pop(stack));
-    while (i < 9 && !full(stack))
-        push(stack, data[i++]);
-    while (!empty(stack))
-        printf("%d\n", pop(stack));
+
+    while (token != Eos) {
+        if (token == Operand) {
+            push(stack, symbol - '0');
+        }
+        else{
+            op2 = pop(stack);
+            op1 = pop(stack);
+
+            switch (token) {
+                case Plus:
+                    push(stack, op1 + op2);
+                    break;
+                case Minus:
+                    push(stack, op1 - op2);
+                    break;
+                case Times:
+                    push(stack, op1 * op2);
+                    break;
+                case Divide:
+                    push(stack, op1 / op2);
+                    break;
+                case Mod:
+                    push(stack, op1 % op2);
+            }
+        }
+        token = getToken(&symbol, &n);
+    }
+
+    return pop(stack);
+}
+
+
+int main() {
+//    slink stack = createStack(5);
+//    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//    int i = 0;
+//    while (!full(stack))
+//        push(stack, data[i++]);
+//    while (!empty(stack))
+//        printf("%d\n", pop(stack));
+//    while (i < 9 && !full(stack))
+//        push(stack, data[i++]);
+//    while (!empty(stack))
+//        printf("%d\n", pop(stack));
+
+    printf("%d", calcPostfix());
 }
 
